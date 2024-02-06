40dc372449d59cd84443e1423fd8411e30e84449
nico <->
2024-02-06 08:56:38 +0100

    korrektur

--- swcs-clean/src/main/java/swcs/clean/car/before/ImmutableCar.java
+++ swcs-clean/src/main/java/swcs/clean/car/before/ImmutableCar.java
@@ -5,35 +5,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 // Alternative solution with record: see RecordCar.java
-class ImmutableCar {
+final class ImmutableCar {
 
     private final String vin;
     private final LocalDate manufactured;
     private final List<String> previousOwners;
 
-    public ImmutableCar(String number, LocalDate manufactured, List<String> previousOwners) {
-        this.vin = number;
+    public ImmutableCar(String vin, LocalDate manufactured, List<String> previousOwners) {
+        this.vin = vin;
         this.manufactured = manufactured;
         this.previousOwners = List.copyOf(previousOwners);
-    }
-
-    public LocalDate manufactured() {
-        return this.manufactured;
     }
 
     public String vin() {
         return this.vin;
     }
 
+    public LocalDate manufactured() {
+        return this.manufactured;
+    }
+
     public List<String> previousOwners() {
-        return List.copyOf(this.previousOwners);
+        return this.previousOwners;
     }
 
     public ImmutableCar addPreviousOwner(String text) {
-        text = text.trim();
         List<String> newPreviousOwners = new ArrayList<>(this.previousOwners);
-        newPreviousOwners.add(text);
-        // todo: Ist hier das List.copyOf notwendig?
+        newPreviousOwners.add(text.trim());
+        // todo: Ist hier das List.copyOf notwendig? Ja, denn Zurückgebene Liste soll auch immutable sein!
         return new ImmutableCar(this.vin, this.manufactured, List.copyOf(newPreviousOwners));
     }
 }
--- swcs-clean/src/main/java/swcs/clean/car/before/RecordCar.java
+++ swcs-clean/src/main/java/swcs/clean/car/before/RecordCar.java
@@ -4,17 +4,23 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-record RecordCar(String number, LocalDate manufactured, List<String> previousOwners) {
+record RecordCar(String vin, LocalDate manufactured, List<String> previousOwners) {
 
     public RecordCar {
         previousOwners = List.copyOf(previousOwners);
     }
 
-    public ImmutableCar addPreviousOwner(String text) {
-        text = text.trim();
-        List<String> newPreviousOwners = new ArrayList<>(this.previousOwners);
-        newPreviousOwners.add(text);
+    // Das wäre nicht die optimale Lösung
+    public static ImmutableCar of(RecordCar car, String newOwner) {
+        List<String> newPreviousOwners = new ArrayList<>(car.previousOwners());
+        newPreviousOwners.add(newOwner.trim());
         // todo: Hier die gleiche Frage wie in ImmutableCar: Ist hier das List.copyOf notwendig?
-        return new ImmutableCar(this.number, this.manufactured, List.copyOf(newPreviousOwners));
+        return new ImmutableCar(car.vin(), car.manufactured(), List.copyOf(newPreviousOwners));
+    }
+
+    // besser:
+    // alternative wäre, das nicht static zu machen und die Parameter direkt aus dem alten car zu nehmen
+    public static RecordCar of(RecordCar car, List<String> previousOwners) {
+        return new RecordCar(car.vin(), car.manufactured(), previousOwners);
     }
 }
